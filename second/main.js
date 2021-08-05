53398b876f9e7490936ba2e4f7be5b3081086ae6
iamidlek <iamidlek@naver.com>
2021-08-05 10:41:43 +0900

    "fix : 리뷰 받은 내용을 바탕으로 수정"
    - jsdocs 설치 및 사용해보기
    - 형변환 확인
    - const 사용과 의미
    - for 대신 고차배열 함수 사용

--- second/main.js
+++ second/main.js
@@ -14,7 +14,7 @@ const data = [
 
 // 사용자 입력 받기
 const line = prompt('최대 금액을 입력해주세요.');
-const amount = +line;
+const amount = Number(line);
 
 // 주어진 금액으로 살 수 있는 가장 비싼 상품을 구함
 const item = getItemByAmount(data, amount);
@@ -27,6 +27,13 @@ const msg = item ?
 alert(msg);
 
 // 아래에 getItemByAmount 함수를 작성하세요.
+
+/**
+ * 소지한 금액으로 구매가능한 가장 비싼 상품을 반환하는 함수
+ * @function getItemByAmount 
+ * @param {Number} amount 소지금액 
+ * @returns {object} {name: '물품명',price: 가격} 을 반환
+ */
 function getItemByAmount(data, amount) {
     // 상품 데이터에 중복되는 동일한 가격을 가지는 상품은 없다고 전제한다.(출력 메세지를 반복하지 않기 때문)
     
@@ -36,41 +43,21 @@ function getItemByAmount(data, amount) {
     if (regex.test(amount)){
         return null
     }
-    // 구매할 수 있는 가장 비싼 상품을 비교하기 위한 배열
-    let compair = [];
-    // 차액이 가장 작은 값을 넣을 변수
-    let max = -Infinity;
-    // 상기의 조건에 부합하는 인덱스 번호를 저장
-    let idx = 0;
-
-    // amount와 상품 가격이 일치할 경우 가장 비싼 상품에 해당하므로 return
-    for (let i = 0; i < data.length; i++) {
-        let com = data[i].price - amount;
-        if (com == 0) {
-            return data[i];
-        } else if (com < 0) { 
-            // 일치하지 않는 경우 구매가능한 상품을 차액과 함께 비교하기 위한 배열에 넣는다
-            compair.push({
-                name: data[i].name,
-                price: com
-            });
+    
+    // amount 보다 같거나 작은 배열을 생성 (구매 가능한 상품 배열)
+    const purchasable = data.filter(function (object) {
+        if (object.price <= amount) {
+            return object
         }
-    }
-
-    // 입력한 금액으로 구매 가능한 상품목록이 없는 경우
-    if (compair.length === 0) {
+    });
+    // 구매 가능한 상품이 없을 경우
+    if (purchasable.length === 0) {
         return null
     }
-    // 상품을 훑으며 가장 차액이 적은 상품을 찾고 인덱스 번호를 구한다
-    for (let k = 0; k < compair.length; k++) {
-        if (max < compair[k].price) {
-            max = compair[k].price
-            idx = k
-        }
-    }
-    // 비교하기 위한 배열엔 차액이 들어있으므로 보정하여 return
-    return {
-        name: compair[idx].name,
-        price: compair[idx].price + amount
-    }
+
+    // 구매 가능한 목록중 가장 큰 price 를 가진 상품을 반환
+    const max = purchasable.reduce(function (previous, current) {
+        return previous.price > current.price ? previous : current;
+    });
+    return max
 }
